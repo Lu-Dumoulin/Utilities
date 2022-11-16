a29d71154ec8edcf7edd842ac67cd261259d9833
Lu-Dumoulin <111343263+Lu-Dumoulin@users.noreply.github.com>
2022-11-16 17:56:34 +0100

    ignore login

--- Code2Cluster.jl
+++ Code2Cluster.jl
@@ -1,17 +1,6 @@
 include("ssh_utilities.jl")
 usingpkg("JSON, Markdown, Dates, DelimitedFiles, CSV, DataFrames")
 
-# It is necessary to use SSH key login, which removes the need for a password for each login, thus ensuring a password-less, automatic login process
-# To do that use
-# `$ ssh-keygen` to generate public and private key files stored in the ~/.ssh directory
-# Then copy the public key in the ~/.ssh/authorized_keys file, on ubuntu you can use
-# `ssh-copy-id -i ~/.ssh/id_rsa.pub user@remote_host`
-# or (if ssh-copy-id not installed)
-# `cat ~/.ssh/id_rsa.pub | ssh username@remote_host "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys" `
-# Eventually, for Windows users, you can manually copy your public key
-# open `~/.ssh/id_rsa.pub` and copy the public key, connect to the remote server using ssh
-# `echo past >> ~/.ssh/authorized_keys` with past the ssh key (Is it also possible to do it by hand with the GUI of FileZilla
-
 # Convert notebook file in ".ext" file (stop conversion at #STOP in notebook cell)
 # ext = optional kwarg ! ex: call `ipnyb2jl(tmpdir, ipynfile, ext=".py")` for python file
 function ipnyb2jl(ipynfile; ext=".jl")
