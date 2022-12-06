227a8a3c71001ac19f4ea7602b98217d2ed0c99a
Lu-Dumoulin <111343263+Lu-Dumoulin@users.noreply.github.com>
2022-12-06 16:57:03 +0100

    remove one L

--- show_pngs_array_v2.jl
+++ show_pngs_array_v2.jl
@@ -1,23 +1,21 @@
-  #####   #####  #     #    ######     #    ####### #     #                            ### #     #    #     #####  #######                                        
- #     # #     # #     #    #     #   # #      #    #     #      ##   #    # #####      #  ##   ##   # #   #     # #          #    # ###### #  ####  #    # ##### 
- #       #       #     #    #     #  #   #     #    #     #     #  #  ##   # #    #     #  # # # #  #   #  #       #          #    # #      # #    # #    #   #   
- #        #####  #     #    ######  #     #    #    #######    #    # # #  # #    #     #  #  #  # #     # #  #### #####      ###### #####  # #      ######   #   
- #             #  #   #     #       #######    #    #     #    ###### #  # # #    #     #  #     # ####### #     # #          #    # #      # #  ### #    #   #   
- #     # #     #   # #      #       #     #    #    #     #    #    # #   ## #    #     #  #     # #     # #     # #          #    # #      # #    # #    #   #   
-  #####   #####     #       #       #     #    #    #     #    #    # #    # #####     ### #     # #     #  #####  #######    #    # ###### #  ####  #    #   #   
+  #####   #####  #     #    ######     #    ####### #     #   
+ #     # #     # #     #    #     #   # #      #    #     #   
+ #       #       #     #    #     #  #   #     #    #     #    
+ #        #####  #     #    ######  #     #    #    #######   
+ #             #  #   #     #       #######    #    #     #  
+ #     # #     #   # #      #       #     #    #    #     #   
+  #####   #####     #       #       #     #    #    #     #  
                                                                                                                                                                                                                                                                                                                    
 # Enter:
 # Path to csv file
-path_to_csv = "F:/2D_P_Q_PQ_3/DF.csv"
+path_to_csv = "F:/2D_P_Q_PQ_6/DF.csv"
 
-
-# ██████╗  ██████╗     ███╗   ██╗ ██████╗ ████████╗    ███╗   ███╗ ██████╗ ██████╗ ██╗███████╗██╗   ██╗    ██████╗ ███████╗██╗     ██╗      ██████╗ ██╗    ██╗
-# ██╔══██╗██╔═══██╗    ████╗  ██║██╔═══██╗╚══██╔══╝    ████╗ ████║██╔═══██╗██╔══██╗██║██╔════╝╚██╗ ██╔╝    ██╔══██╗██╔════╝██║     ██║     ██╔═══██╗██║    ██║
-# ██║  ██║██║   ██║    ██╔██╗ ██║██║   ██║   ██║       ██╔████╔██║██║   ██║██║  ██║██║█████╗   ╚████╔╝     ██████╔╝█████╗  ██║     ██║     ██║   ██║██║ █╗ ██║
-# ██║  ██║██║   ██║    ██║╚██╗██║██║   ██║   ██║       ██║╚██╔╝██║██║   ██║██║  ██║██║██╔══╝    ╚██╔╝      ██╔══██╗██╔══╝  ██║     ██║     ██║   ██║██║███╗██║
-# ██████╔╝╚██████╔╝    ██║ ╚████║╚██████╔╝   ██║       ██║ ╚═╝ ██║╚██████╔╝██████╔╝██║██║        ██║       ██████╔╝███████╗███████╗███████╗╚██████╔╝╚███╔███╔╝
-# ╚═════╝  ╚═════╝     ╚═╝  ╚═══╝ ╚═════╝    ╚═╝       ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝╚═╝        ╚═╝       ╚═════╝ ╚══════╝╚══════╝╚══════╝ ╚═════╝  ╚══╝╚══╝ 
-   
+# ██████╗  ██████╗     ███╗   ██╗ ██████╗ ████████╗    ███╗   ███╗ ██████╗ ██████╗ ██╗███████╗██╗   ██╗    ██████╗ ███████╗██╗      ██████╗ ██╗    ██╗
+# ██╔══██╗██╔═══██╗    ████╗  ██║██╔═══██╗╚══██╔══╝    ████╗ ████║██╔═══██╗██╔══██╗██║██╔════╝╚██╗ ██╔╝    ██╔══██╗██╔════╝██║     ██╔═══██╗██║    ██║
+# ██║  ██║██║   ██║    ██╔██╗ ██║██║   ██║   ██║       ██╔████╔██║██║   ██║██║  ██║██║█████╗   ╚████╔╝     ██████╔╝█████╗  ██║     ██║   ██║██║ █╗ ██║
+# ██║  ██║██║   ██║    ██║╚██╗██║██║   ██║   ██║       ██║╚██╔╝██║██║   ██║██║  ██║██║██╔══╝    ╚██╔╝      ██╔══██╗██╔══╝  ██║     ██║   ██║██║███╗██║
+# ██████╔╝╚██████╔╝    ██║ ╚████║╚██████╔╝   ██║       ██║ ╚═╝ ██║╚██████╔╝██████╔╝██║██║        ██║       ██████╔╝███████╗███████╗╚██████╔╝╚███╔███╔╝
+# ╚═════╝  ╚═════╝     ╚═╝  ╚═══╝ ╚═════╝    ╚═╝       ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝╚═╝        ╚═╝       ╚═════╝ ╚══════╝╚══════╝ ╚═════╝  ╚══╝╚══╝ 
 
 include("../Utilities/julia_utilities.jl")
 usingpkg("Makie, JLD, Printf, FixedPointNumbers, FileIO, Base64, Colors, Images, CSV, DataFrames, Interact, Blink, WebIO")
@@ -28,9 +26,11 @@ screen_size = displaysize()
 hor_size = screen_size[1]
 vert_size = screen_size[2]
 hv = string(vert_size*0.85, "px")
-hh = string(hor_size*0.8, "px")
+hh = string(hor_size*0.75, "px")
 wh = floor(Int, hor_size*0.9)
-wv = floor(Int, vert_size*0.9)
+wv = floor(Int, vert_size*0.90)
+buth = string(hor_size*(0.9-0.75),"px")
+
 
 df = CSV.read(dir*name_csv, DataFrame)[:,1:end]
 symb = Sys.iswindows() ? '\\' : '/'
@@ -68,14 +68,20 @@ sl_name = []
 var_list_sl = []
 check_list =[] 
 check_name = []
-# var_list_check = []
 N_wid = 0
 for i in 1:Ncol-1
     if length(tab_list[i]) > 1
-        symbolname = Symbol("sl_$(var_list[i])")
-        @eval $symbolname = $(slider(tab_list[i], label="$(var_list[i])"))
-        @eval push!(sl_list, $symbolname )
-        push!(sl_name, "sl_$(var_list[i])")
+        if isa((tab_list[i][1]), AbstractString)
+            symbolname = Symbol("sl_$(var_list[i])")
+            @eval $symbolname = $(togglebuttons(tab_list[i], label= "$(var_list[i])") ) 
+            @eval push!(sl_list, $symbolname )
+            push!(sl_name, "sl_$(var_list[i])")
+        else
+            symbolname = Symbol("sl_$(var_list[i])")
+            @eval $symbolname = $(slider(tab_list[i], label= "$(var_list[i])") ) 
+            @eval push!(sl_list, $symbolname )
+            push!(sl_name, "sl_$(var_list[i])")
+        end
         symbolname = Symbol("check_$(var_list[i])")
         @eval $symbolname = $(checkbox())
         @eval push!(check_list, $symbolname)
@@ -91,10 +97,8 @@ radio_dir = Interact.radiobuttons( tab_fig_dir )
 radio_png = Interact.togglebuttons( OrderedDict( "PNG"=> "PNG", "GIF"=> "GIF" ), value="PNG")
 ui = Observable{Any}()
 global fn = Observable{Any}(string(df[1,:fn])*"/")
-fnpict = Observable{Any}("filename")
 pict = Observable{Any}()
 sl = slider(1:2, value=1)
-
 
 function getpict(t)
     if t > Nt
@@ -104,9 +108,29 @@ function getpict(t)
     end
 end
 
+# for wid in 1:N_wid
+#     eval(quote
+#             onany($(sl_list[wid]), $(check_list[wid])) do x, y
+#                 dist = zeros(Nrow)
+#                 for r=1:Nrow
+#                     for c=1:N_wid
+#                         dist[r] += Int(df[r, Symbol(var_list_sl[c])] == sl_list[c][])*(1 + Int(check_list[c][])*100)
+#                     end
+#                 end
+#                 idx = findmax(dist)[2]
+#                 for c=1:N_wid
+#                     if sl_list[c][] != df[idx, Symbol(var_list_sl[c])]
+#                         sl_list[c][] = df[idx, Symbol(var_list_sl[c])]
+#                     end
+#                 end
+#                 global fn[] = string(df[idx,:fn])*"/"
+#             end
+#         end)
+# end
+
 for wid in 1:N_wid
     eval(quote
-            onany($(sl_list[wid]), $(check_list[wid])) do x, y
+            on($(check_list[wid])) do x
                 dist = zeros(Nrow)
                 for r=1:Nrow
                     for c=1:N_wid
@@ -114,7 +138,27 @@ for wid in 1:N_wid
                     end
                 end
                 idx = findmax(dist)[2]
-                for c=1:N_sl
+                for c=1:N_wid
+                    if sl_list[c][] != df[idx, Symbol(var_list_sl[c])]
+                        sl_list[c][] = df[idx, Symbol(var_list_sl[c])]
+                    end
+                end
+                global fn[] = string(df[idx,:fn])*"/"
+            end
+        end)
+end
+
+for wid in 1:N_wid
+    eval(quote
+            on($(sl_list[wid])) do x
+                dist = zeros(Nrow)
+                for r=1:Nrow
+                    for c=1:N_wid
+                        dist[r] += Int(df[r, Symbol(var_list_sl[c])] == sl_list[c][])*(1 + Int(check_list[c][])*100)
+                    end
+                end
+                idx = findmax(dist)[2]
+                for c=1:N_wid
                     if sl_list[c][] != df[idx, Symbol(var_list_sl[c])]
                         sl_list[c][] = df[idx, Symbol(var_list_sl[c])]
                     end
@@ -125,24 +169,19 @@ for wid in 1:N_wid
 end
 
 function getui(fn, radio_dir, radio_png, sl)
-    global dirf = joinpath(dir,fn)
-    if isdir(dirf) && length(readdir(dirf*radio_dir)) > 0
-        global dirf *= radio_dir
+    global dirf = joinpath(dir,fn) 
+    dirf *= radio_dir
+    if isdir(dirf) && length(readdir(dirf)) > 0
         if radio_png == "PNG"
-            if isdir(dirf)
-                global lpict = readdir(dirf)
-                global Nt = length(lpict)
-                id = sl[]
-                global sl = Interact.slider(1:Nt, value=id, label="t")
-                map!(getpict, pict, sl)
-                return dom"div"(style = Dict("width" => hh,
-                                       "height" => hv), pict, sl)
-            else
-                return dom"div"()
-            end
+            global lpict = readdir(dirf)
+            global Nt = length(lpict)
+            id = sl[]
+            global sl = Interact.slider(1:Nt, value=id, label="t")
+            map!(getpict, pict, sl)
+            return dom"div"(style = Dict("width" => hh, "height" => hv), pict, sl)
         else
             gif_path = filter!(endswith(".gif"),readdir(dirf))
-            if length(gif_path) ==0
+            if length(gif_path) == 0
                 return dom"div"()
             else
                 return dom"div"(showgifb(dirf*gif_path[1]))
@@ -155,4 +194,6 @@ end
 
 map!(getui, ui, fn, radio_dir, radio_png, sl)
 
-eval(Meta.parse(string("""body!(w, dom"div"(hbox( vbox(radio_dir, radio_png, """)*join([ " hbox( $(sl_name[i]), $(check_name[i]) ), " for i in 1:N_wid ])[1:end-1]*string("), ui)))")))
+eval(Meta.parse(string("""body!(w, dom"div"( hbox( vbox(radio_dir, radio_png, """)*join([ " hbox( $(sl_name[i]), $(check_name[i]) ), " for i in 1:N_wid ])[1:end-1]*string(") , ui)))")))
+
+# eval(Meta.parse(string("""body!(w, dom"div"(hbox( vbox(radio_dir, radio_png, """)*join([ " hbox( $(sl_name[i]), $(check_name[i]) ), " for i in 1:N_wid ])[1:end-1]*string("), ui)))")))
